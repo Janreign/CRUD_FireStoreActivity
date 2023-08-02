c3189e09051e93c81475762c2bf85aad23d0cf8a
Janreign <janreign.tibs@gmail.com>
2023-08-03 01:45:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/crud_firestoreactivity/util/UserData.kt
@@ -0,0 +1,8 @@
+package com.example.crud_firestoreactivity.util
+
+data class UserData(
+    var userID: String = "",
+    var bandMembername: String = "",
+    var position: String = "",
+    var age: Int = 0,
+)
